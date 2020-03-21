352e680c69a2c4350f01d657252938161d2e7fd2
Subilan <suyuanliang1995@gmail.com>
2020-03-21 19:57:54 +0800

    添加 Logo Component

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+    <circle class="a" cx="250" cy="250" r="71" />
+    <path class="a" d="M142.53,324.94A131,131,0,0,1,324.94,142.53" />
+    <path class="a" d="M367.82,99.66c-75-58.89-183.79-53.8-252.88,15.28S40.77,292.86,99.66,367.82" />
+    <path class="a" d="M135.38,402.8c74.83,56.27,181.57,50.37,249.68-17.74s74-174.85,17.74-249.68" />
+    <path class="a" d="M359.75,178.44A131,131,0,0,1,178.44,359.75" />
+    <path class="a" d="M186.24,281.23a71,71,0,0,1,95-95" />
+    <path class="a" d="M324.94,142.53A131,131,0,0,0,142.53,324.94" />
+    <path class="a" d="M178.44,359.75A131,131,0,0,0,359.75,178.44" />
+    <path class="a" d="M315.53,222.66a71,71,0,0,1-92.87,92.87" />
+  </svg>
+</template>
+
+<style scoped>
+.a {
+  fill: none;
+  stroke: #30adf4;
+  stroke-linecap: round;
+  stroke-miterlimit: 10;
+  stroke-width: 18px;
+}
+</style>
