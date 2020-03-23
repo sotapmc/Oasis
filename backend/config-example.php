eb2779014f805d9fb106c7c8ad6a9a781d86c255
Subilan <suyuanliang1995@gmail.com>
2020-03-23 13:35:17 +0800

    Use config example instead

---
+++ backend/config-example.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * config-example.php
+ * 这是一个配置文件，用来控制 Oasis 在各方面的表现，
+ * 你可以根据注释和自己的理解修改这些内容，使其适合您的使用。
+ * 
+ * 重要：在使用时请将文件名更改为 config.php 即去掉 -example
+ */
+
+require "functions/config.php";
+
+return new Config([
+    // 数据库相关设置
+    "mysql" => [
+        "host" => "localhost", // 主机名
+        "username" => "root", // 用户名
+        "password" => "" // 密码
+    ],
+]);
