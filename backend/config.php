6487b393136f43f10140d094b2167dbf6ef3b528
Subilan <suyuanliang1995@gmail.com>
2020-03-23 10:11:18 +0800

    基础配置文件

---
+++ backend/config.php
@@ -0,0 +1,11 @@
+<?php
+
+require "functions/config.php";
+
+return new Config([
+    "mysql" => [
+        "host" => "localhost",
+        "username" => "root",
+        "password" => ""
+    ]
+]);
