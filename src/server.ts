46772cf07967f6a27832f56b0a2b2ee6f5a57b8a
Subilan <suyuanliang1995@gmail.com>
2020-03-24 09:38:24 +0800

    范式适配：添加 getcfg 方法迅速获得配置文件中的配置

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosProxyConfig } from 'axios';
 import qs from 'qs';
 
 class Server {
     public static axios = axios;
 
-    public static post(data: Object, callback: Function): void {
-        axios.post("/backend/data-transfer.php", qs.stringify(data), {
+    public static post(action: string, data: object | string, callback: Function): void {
+        axios.post("/backend/data-transfer.php", qs.stringify({
+            action,
+            data
+        }), {
             method: "POST"
         }).then(result => {
             callback(result);
+        }).catch(reason => {
+            console.error(reason);
+        })
+    }
+
+    public static getcfg(cfg: string | Array<string>, callback: Function) {
+        axios.post("/backend/data-transfer.php", qs.stringify({
+            action: "get-config",
+            data: cfg
+        }), {
+            method: "POST"
+        }).then(result => {
+            callback(result.data);
         }).catch(reason => {
             console.error(reason);
         })
