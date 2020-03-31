3bbe8fe479602949b4b3b4f2bfafb4fbb74e581f
Subilan <suyuanliang1995@gmail.com>
2020-03-31 21:47:58 +0800

    Add config filter (beta)

--- backend/functions/config-controller.php
+++ backend/functions/config-controller.php
@@ -1,16 +1,34 @@
 <?php
 
-class Config {
+class Config
+{
     private $config;
-    public function __construct(array $conf) {
+    private $path;
+    public function __construct(array $conf)
+    {
         $this->config = $conf;
     }
 
-    public function get(string $path) {
+    public function get(string $path)
+    {
+        $this->path = $path;
         $arr = explode(".", $path);
         for ($i = 0; $i < count($arr); $i++) {
             $result = $i === 0 ? $this->config[$arr[$i]] : $result[$arr[$i]];
         }
-        return $result;
+        return $this->filter($result);
+    }
+
+    public function filter(string $value)
+    {
+        switch ($this->path) {
+            case "oasis.max-app-per-page":
+                if (!($value >= 5 && $value <= 50)) {
+                    return 5;
+                }
+
+                // no default case needed
+        }
+        return $value;
     }
 }
