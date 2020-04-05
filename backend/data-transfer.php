003b2cc63899d6b9ab9784f5c8efd5660dfca645
Subilan <suyuanliang1995@gmail.com>
2020-04-05 19:49:34 +0800

    submit n get review methods

--- backend/data-transfer.php
+++ backend/data-transfer.php
@@ -26,20 +26,20 @@ switch ($action) {
             } else {
                 echo json_encode([
                     "error" => $ver->error,
-                    "reason" => $ver->reason
+                    "reason" => $ver->reason,
                 ], JSON_UNESCAPED_UNICODE);
             }
         } else {
             echo $ver->reason;
         }
-    break;
+        break;
 
     case "get-agreement":
         require_once "vendor/parsedown.php";
         $parser = new Parsedown();
         $agreement = file_get_contents("agreement.md");
         echo $parser->text($agreement);
-    break;
+        break;
 
     case "get-applications":
         $limit = $data["page"];
@@ -53,25 +53,51 @@ switch ($action) {
         } else {
             echo $conn->getError();
         }
-    break;
+        break;
 
-    case "remove-application":
+    case "get-application":
         require_once "functions/database.php";
         $conn = new DBController($cfg, "oasis");
         $id = $data["id"];
+        $result = $conn->query("SELECT * FROM applications WHERE id='$id'");
+        if ($conn->isOK()) {
+            echo json_encode($result->fetch_array(MYSQLI_ASSOC), JSON_UNESCAPED_UNICODE);
+        } else {
+            echo $conn->getError();
+        }
+        break;
+
+    case "toggle-application":
+        require_once "functions/database.php";
+        $conn = new DBController($cfg, "oasis");
+        $id = $data["id"];
+        $action = $data["action"];
+        $status = $action === "remove" ? "yes" : "no";
         if (is_array($id)) {
             foreach ($id as $value) {
-                $conn->query("UPDATE applications set removed='yes' WHERE id='$value'");
+                $conn->query("UPDATE applications SET removed='$status' WHERE id='$value'");
             }
         } else {
-            $conn->query("UPDATE applications SET removed='yes' WHERE id='$id'");
+            $conn->query("UPDATE applications SET removed='$status' WHERE id='$id'");
         }
         if ($conn->isOK()) {
             echo 'ok';
         } else {
             echo $conn->getError();
         }
-    break;
+        break;
+
+    case "update-application-status":
+        require_once "functions/database.php";
+        $conn = new DBController($cfg, "oasis");
+        extract($data);
+        $conn->query("UPDATE applications SET status='$status' WHERE id='$id'");
+        if ($conn->isOK()) {
+            echo "ok";
+        } else {
+            echo $conn->getError();
+        }
+        break;
 
     case "get-max-page":
         require_once "functions/database.php";
@@ -80,7 +106,7 @@ switch ($action) {
         if ($conn->isOK()) {
             echo ceil($result->num_rows / $cfg->get("oasis.max-app-per-page")); // min value 0
         }
-    break;
+        break;
 
     case "get-config":
         if (is_array($data)) {
@@ -92,7 +118,7 @@ switch ($action) {
             $result = $cfg->get($data);
         }
         echo is_array($result) ? json_encode($result, JSON_UNESCAPED_UNICODE) : $result;
-    break;
+        break;
 
     case "get":
         require "functions/data-getter.php";
@@ -109,5 +135,69 @@ switch ($action) {
             $Getter->get($value);
             echo $Getter->getResult();
         }
-    break;
+        break;
+
+    case "submit-review":
+        require "functions/database.php";
+        $conn = new DBController($cfg, "oasis");
+        $id = $data["id"];
+        $r = $conn->query("SELECT * FROM review WHERE id='$id'");
+        if ($r->num_rows > 0) {
+            foreach ($data as $key => $value) {
+                if ($key === "id") {
+                    continue;
+                }
+                $conn->query("UPDATE review SET $key='$value' WHERE id='$id'");
+            }
+        } else {
+            extract($data);
+            $conn->query("INSERT INTO review (id, username, comment, overall_rank, basic_rank)
+            VALUES ('$id', '$username', '$comment', '$overall_rank', '$basic_rank')");
+        }
+        if ($data["overall_rank"] + $data["basic_rank"] >= 12) {
+            $conn->query("UPDATE applications SET status='passed' WHERE id='$id' AND removed='no'");
+        } else {
+            $conn->query("UPDATE applications SET status='invalid' WHERE id='$id' AND removed='no'");
+        }
+        if ($conn->isOK()) {
+            echo "ok";
+        } else {
+            echo $result->getError();
+        }
+        break;
+
+    case "get-review":
+        require "functions/database.php";
+        $conn = new DBController($cfg, "oasis");
+        $id = $data["id"];
+        $result = $conn->query("SELECT * FROM review WHERE id='$id'");
+        if ($conn->isOK()) {
+            echo json_encode($result->fetch_array(MYSQLI_ASSOC), JSON_UNESCAPED_UNICODE);
+        } else {
+            echo $conn->getError();
+        }
+        break;
+
+    case "toggle-spam":
+        require "functions/database.php";
+        $conn = new DBController($cfg, "oasis");
+        $id = $data["id"];
+        $r = $conn->query("SELECT spam FROM review WHERE id='$id'");
+        if ($r->num_rows > 0) {
+            if ($r->fetch_assoc()["spam"] === "yes") {
+                $conn->query("UPDATE review SET spam='no' WHERE id='$id'");
+            } else {
+                $conn->query("UPDATE review SET spam='yes' WHERE id='$id'");
+            }
+        } else {
+            extract($data);
+            $conn->query("INSERT INTO review (id, comment, overall_rank, basic_rank, spam)
+            VALUES ('$id', '', '0', '0', 'yes')");
+        }
+        if ($conn->isOK()) {
+            echo "ok";
+        } else {
+            echo $result->getError();
+        }
+        break;
 }
